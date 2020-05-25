153bfe186d21eba26bad2bfb58074fe01a0fbfd1
Tesla-hash <t3sla2020@yandex.ru>
2020-05-25 04:04:19 +0000

    ver 1.0

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,105 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title>
+          Telegram CMS
+        </q-toolbar-title>
+
+        <div>Made by Tesla</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+            v-model="leftDrawerOpen"
+            show-if-above
+            :width="200"
+            :breakpoint="600"
+          >
+          <q-scroll-area style="height: calc(100% - 150px); margin-top: 150px; border-right: 1px solid #ddd">
+              <q-list padding>
+                <q-item clickable v-ripple>
+                  <q-item-section avatar>
+                    <q-icon name="inbox" />
+                  </q-item-section>
+
+                  <q-item-section>
+                    Inbox
+                  </q-item-section>
+                </q-item>
+
+                <q-item active clickable v-ripple>
+                  <q-item-section avatar>
+                    <q-icon name="star" />
+                  </q-item-section>
+
+                  <q-item-section>
+                    Star
+                  </q-item-section>
+                </q-item>
+
+                <q-item clickable v-ripple>
+                  <q-item-section avatar>
+                    <q-icon name="send" />
+                  </q-item-section>
+
+                  <q-item-section>
+                    Send
+                  </q-item-section>
+                </q-item>
+
+                <q-item clickable v-ripple>
+                  <q-item-section avatar>
+                    <q-icon name="drafts" />
+                  </q-item-section>
+
+                  <q-item-section>
+                    Drafts
+                  </q-item-section>
+                </q-item>
+              </q-list>
+            </q-scroll-area>
+
+            <q-img class="absolute-top" src="https://cdn.quasar.dev/img/material.png" style="height: 150px">
+              <div class="absolute-bottom bg-transparent">
+                <q-avatar size="56px" class="q-mb-sm">
+                  <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+                </q-avatar>
+                <div class="text-weight-bold">Main Admin</div>
+                <div>@admin</div>
+              </div>
+            </q-img>
+    </q-drawer>
+    <q-page-container style="padding-left:10px; padding-top:0px;">
+      <router-view />
+      </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink';
+import DataService from "../services/DataService";
+
+export default {
+  components: {
+    EssentialLink
+
+  },
+  data(){
+    return{
+      leftDrawerOpen:false
+    }
+  }
+
+}
+
+</script>
